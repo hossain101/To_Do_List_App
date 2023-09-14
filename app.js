e0268cb0721ab951da14cbbc7344b9f856e8ac1f
shoaib <shoaibhossain24@gmail.com>
2023-09-14 10:35:50 +0600

    add port

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.post("/", function (req, res) {
 });
 
 // launch ======================================================================
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server listening on port 3000");
 });
